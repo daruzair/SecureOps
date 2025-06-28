12b790097245b80aeb6cb9044472b3f27ec9dbea
Uzair Shafi <er.uzairshafi@outlook.com>
2025-06-28 09:52:29 +0530

    Added Test Project

---
+++ Test/Controllers/TokenController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Test.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TokenController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("a-string-secret-at-least-256-bits-long"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: "your-app",
+            audience: "your-app-users",
+            claims: new[] { new Claim("UserId", "John"), new Claim("ManagePermissions","true") },
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: creds);
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return Ok(new { token = jwt });
+    }
+}
